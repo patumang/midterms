0c022ae88f9bfb24abc111b86772899b460ee9ec
jvblum <jvmell.skch@gmail.com>
2021-10-18 02:01:21 -0400

    refactor routes/query-helpers/insert

---
+++ routes/query-helpers/insert.js
@@ -0,0 +1,89 @@
+module.exports = db => {
+
+  const uniqueUrlGenerator = () => {
+    return Math.random().toString(20).substr(2, 10);
+  };
+
+  const insertEventInDb = (body) => {
+
+    const event_url = uniqueUrlGenerator();
+    const queryParams = [
+      body.organizer_name,
+      body.organizer_email,
+      body.event_title,
+      body.event_desc,
+      body.event_venue,
+      event_url
+    ];
+
+    let queryString = `
+    INSERT INTO events (creator_name, creator_email, title, description, venue, unique_url)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING id;
+    `;
+
+    return db.query(queryString, queryParams)
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const insertTimingsInDb = (body, eventID) =>{
+
+    let queryString = `
+    INSERT INTO timings (event_id, date, start_time, end_time)
+    VALUES ($1, $2, $3, $4);
+    `;
+
+    // bug: wrong loop if there's only one timings input set;
+    // possible solution: if time_slot is not array, then don't loop;
+    // note: date needs validation; does not accept wrong input type
+    if (Array.isArray(body.time_slot_date)) {
+
+      //create new promise each loop and push to this array
+      let promises = [];
+
+      for (let i = 0; i < body.time_slot_date.length; i++){
+        let queryParams = [
+          eventID,
+          body.time_slot_date[i],
+          body.time_slot_start_time[i],
+          body.time_slot_end_time[i]
+        ];
+
+        promises.push(db.query(queryString, queryParams));
+      }
+
+      Promise.all(promises)
+      .catch((err) => console.log(err.message))
+      .finally(() => db.end());
+
+    } else {
+
+      let queryParams = [
+        eventID,
+        body.time_slot_date,
+        body.time_slot_start_time,
+        body.time_slot_end_time
+      ];
+
+      db.query(queryString, queryParams)
+      .catch(err => console.log(err.message));
+
+    }
+
+  };
+
+  const insertAllInDb = (body) => {
+
+    insertEventInDb(body)
+    .then((res) => {
+      insertTimingsInDb(body, res.rows[0].id);
+    })
+    .catch(err => console.log(err.message));
+
+  };
+
+  return { insertAllInDb };
+
+};
