94b842bd1598138415ecd73eba86d533c5b82806
Xavier Savoie <xaviersavoie@gmail.com>
2021-10-21 17:37:45 +0000

    setup mail.js to send basic email, and added nodemailer + nodemailer-mailgun-transport dependencies

---
+++ mail.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+const auth = {
+    auth: {
+      api_key: 'a84a195b9a497158863660f35bc94b32-2bf328a5-68063dd4',
+      domain: 'sandbox4abfa3f71422448a9ceecbb0b048d2f0.mailgun.org'
+    }
+};
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+
+const sendMail = (email, creatorName, uniqueUrl, cb) => {
+
+  const mailOptions = {
+    from: 'schoodle.midterm@gmail.com',
+    to: email,
+    subject: 'Testing',
+    text: `Hello ${creatorName}, your meeting url is localhost:8080/event/${uniqueUrl}`
+  };
+
+  transporter.sendMail(mailOptions, function(err, data) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
+    }
+  });
+
+};
+
+module.exports = sendMail;
